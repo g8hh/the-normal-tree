092064df2df39d4f22708ab0b3eae9ef46240c62
aden0216 <79700296+aden0216@users.noreply.github.com>
2021-06-20 19:02:08 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1126,7 +1126,7 @@ addLayer("Hardcap", {
 	tooltip: "Hardcap",
     tabFormat: [
 		"blank", "blank", "blank",
-        ["raw-html", "<h1><a href=https://docs.google.com/document/d/1oT5siVj4lT8nnmHjPmAiSQL1NVSmNXQT8bpgUUqjBkM/edit target=_blank>Hardcap table</a></h1>"],
+        ["raw-html", "<h1><a href=https://docs.google.com/document/d/1oT5siVj4lT8nnmHjPmAiSQL1NVSmNXQT8bpgUUqjBkM/edit target=_blank>Hardcap table</a></h1><br><h1><a href=https://docs.google.com/document/d/1Re0J_14Ivl_ON4CyqXWk-6nwZnxFfzoG3rxbQqe4Tgg/edit target=_blank>Save bank</a></h1>"],
 	],
 })
 addLayer("A", {
