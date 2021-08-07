b6c3977556cebba6a034ffb6ff74ae194d279342
aden0216 <79700296+aden0216@users.noreply.github.com>
2021-08-07 18:46:36 +0800

    j

--- js/layers.js
+++ js/layers.js
@@ -1,28 +1,3 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
-    }},
-    color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
-})
+addLayer("p", {name: "prestige", symbol: "P",position: 0, startData() { return {unlocked: true,points: new Decimal(0),}},color: "#4BDC13",requires: new Decimal(10), resource: "prestige points",baseResource: "points", baseAmount() {return player.points},type: "normal",exponent: 0.5,
+    gainMult() { mult=new Decimal(1)
+        return mult},gainExp() {return new Decimal(1)},row: 0,hotkeys: [{key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},],upgrades: {11: {title: "Simple boost",description: "points x2",cost: new Decimal(1),},12: {title: "point booster",description: "points boost themselves.",cost: new Decimal(1),effect(){return player.points.add(1).log(10).add(1)},effectDisplay(){return player.points.add(1).log(10).add(1)+"x"},},13: {title: "prestige booster",description: "prestige point boost point gain.",cost: new Decimal(1),effect(){return player.p.points.add(1).pow(0.5)},effectDisplay(){return player.p.points.add(1).pow(0.5)+"x"},},14: {title: "Over power",description: "point boost point gain.",cost: new Decimal(1),effect(){return player.points.add(1).pow(0.5)},effectDisplay(){return player.points.add(1).pow(0.5)+"x"},},},layerShown(){return true}})
--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The speed of tree",
+	id: "speed",
+	author: "3^3=7",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
